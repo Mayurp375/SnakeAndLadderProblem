1bfb4017029376b7ab951ff2a5bfeaab6ebf6fdb
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-12 13:54:54 +0530

    start from zero

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to  Snake & Ladder Problem");
 
+        int position = 0;
+        System.out.println("position" + position);
+
     }
 }
