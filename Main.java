36948a491e93f131b49530eeef05c380778a9add
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-12 12:57:03 +0530

    welcome to snack and ladder

---
+++ Main.java
@@ -0,0 +1,8 @@
+package classProblemByAmolMateSir.Day4PracticeProblem.SnakeAndLadderProblem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to  Snake & Ladder Problem");
+
+    }
+}
