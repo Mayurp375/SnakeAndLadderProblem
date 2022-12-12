cb92f5d2bab4e3feb7140e17a901bf52e156fb9e
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-12 14:03:18 +0530

    die Role

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
 package classProblemByAmolMateSir.Day4PracticeProblem.SnakeAndLadderProblem;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to  Snake & Ladder Problem");
 
         int position = 0;
-        System.out.println("position" + position);
 
+        Random random = new Random();
+        int die = random.nextInt(7);
+        if (die >= 0) {
+            position += die;
+        }
+        System.out.println(position);
     }
 }
